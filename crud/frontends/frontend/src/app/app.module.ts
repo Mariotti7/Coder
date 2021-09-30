6b3bf82285eaf404e864de8ca251c743fb7c1e7c
Leandro <midiasmart99@gmail.com>
2021-09-29 23:03:23 -0300

    Header | Footer | NavBar

--- crud/frontends/frontend/src/app/app.module.ts
+++ crud/frontends/frontend/src/app/app.module.ts
@@ -6,19 +6,28 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar'
-import { MatIconModule } from '@angular/material/icon'
+import { MatIconModule } from '@angular/material/icon';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component'
+
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatListModule } from '@angular/material/list'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
